7de464ef12d24a522df5669aa46adb4677148d7c
Memory-Wabwile <memorywabwile@gmail.com>
2022-03-23 13:12:55 +0300

    add the tags class

--- news/models.py
+++ news/models.py
@@ -5,3 +5,23 @@ class Editor(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+
+
+    class Meta:
+        ordering = ['first_name']
+
+    # try:
+    #     editor = Editor.objects.get(email = 'example@gmail.com')
+    #     print('Editor found')
+    # except DoesNotExist:
+    #     print('Editor was not found')
+
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
