00e723d721ad0ae4b4e5451f09c642607c1ebb18
Memory-Wabwile <memorywabwile@gmail.com>
2022-03-24 04:19:25 +0300

    create a template for displaying a single article

--- news/urls.py
+++ news/urls.py
@@ -7,7 +7,8 @@ urlpatterns=[
     # path('',views.welcome,name='welcome'),
     path('',views.news_today,name='newsToday'),
     re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
-    path('search/', views.search_results, name='search_results')
+    path('search/', views.search_results, name='search_results'),
+    re_path(r'^article/(\d+)',views.article,name ='article')
 ]
 
 if settings.DEBUG:
