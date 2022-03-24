f201673f505e11de587bd77e0ce248a71f77766f
Memory-Wabwile <memorywabwile@gmail.com>
2022-03-24 04:05:05 +0300

    update  the template file with the displaying images column

--- news/urls.py
+++ news/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path,re_path
 from . import views
 
@@ -7,3 +9,6 @@ urlpatterns=[
     re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
     path('search/', views.search_results, name='search_results')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
